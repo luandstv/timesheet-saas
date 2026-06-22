5fe9b77b35f59afa67be3609d676633212141e79
Luan alves <luanalves.dev@gmail.com>
2026-06-22 05:02:08 -0300

    chore: prepara arquivos antes de rodar o codemod do middleware

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware";
+
+const publicRoutes = ["/login", "/resgister"];
+
+export async function middleware(request: NextRequest) {
+  const { supabaseResponse, user } = await updateSession(request);
+
+  const pathname = request.nextUrl.pathname;
+
+  if (!user && !publicRoutes.includes(pathname)) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  if (user && publicRoutes.includes(pathname)) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/dashboard";
+    return NextResponse.redirect(url);
+  }
+
+  return supabaseResponse;
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
